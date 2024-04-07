b2c8b6019fa22c126ce4e5b479dd15de130c86e1
Your Name <you@example.com>
2024-04-07 11:53:30 -0500

    working on overlap section also installed dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    plugins: [
+        new Dotenv()
+    ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'    
