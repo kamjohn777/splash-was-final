7c42ca553daa30d59d06ceb81c288aea15926a5f
Kameron Johnson <kamjohn79.kdj@gmail.com>
2024-06-25 20:35:55 -0500

    removed process envs

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,11 @@
 const path = require('path');
-const webpack = require('webpack');
-require('dotenv').config();
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-            'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-            'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-            'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-            'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-            'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-            'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
-        })
+        new Dotenv()
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
